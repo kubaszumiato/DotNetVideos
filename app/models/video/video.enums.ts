e69b2c9657d9c100e34a45771e83b228eb4fcf67
Jakub Szumiato <szumiato@hotmail.com>
2016-03-16 23:29:24 +0100

    reorganizing models a bit

---
+++ app/models/video/video.enums.ts
@@ -0,0 +1,10 @@
+enum VideoType {
+    YouTube = 1,
+    Vimeo,
+    Channel9,
+}
+
+export {VideoType};
+
+//let type = VideoType.YouTube
+//let videoTypeString = VideoType[type]; // displays YouTube
--- app/models/video/video.model.ts
+++ app/models/video/video.model.ts
@@ -1,4 +1,4 @@
-///<reference path="../../typings/main.d.ts"/>
+///<reference path="../../../typings/main.d.ts"/>
 
 // ```
 // video.model.js
@@ -14,10 +14,12 @@
 // Note: MongoDB will autogenerate an _id for each Todo object created
 
 // Grab the Mongoose module
-//import mongoose = require('mongoose');
 
 import mongoose = require('mongoose');
-//import Tag from '../entities/tag';
+import { Tag } from './video.tag';
+import { VideoType } from './video.enums';
+import User from '../user/user.model';
+
 // Create a `schema` for the `video` object
 
 export interface IVideo extends mongoose.Document {
@@ -25,23 +27,26 @@ export interface IVideo extends mongoose.Document {
   title: { type : String },
   //each video must have an Url, whatever the place of publication
   url: { type : String},  
+  //code defines the unique identifier of a movie within a given website, usually it's a part of URL
+  code: { type : String},
   //mediaType describes what kind of movie is that (what type of file preciesly speaking)
   mediaType: { type : String},  
   //length in seconds
   length: { type : Number},  
-  //each movie may be tagged using a tag
-  //to check this type declaration
-  ///todo!
-  //tags: { type : Tag | Array<Tag>},  
+  //videoType declares the website the movie is coming from, eg. YouTube
+  videoType: { type : VideoType}
+  //tags for movies, assigned by movie creator/importer
+  tags: { type : Tag | Array<Tag>},    
   //internal rating by dotnet-videos users
   rating: { type : Number},  
   //who uploaded the movie
-  //owner: {type: User}
+  owner: {type: User}
 };
 
 export const VideoSchema = new mongoose.Schema({
   title: { type : String },
   url: { type : String},  
+  code: { type : String},
   mediaType: { type : String},  
   length: { type : Number},  
   rating: { type : Number},  
